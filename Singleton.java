ea92654c868d0b5bb13379cff3cc3307c8fc9f25
Dilifenjin <1229927330@qq.com>
2021-09-26 16:55:13 +0800

    第一次提交

---
+++ Singleton.java
@@ -0,0 +1,16 @@
+ class Singleton{
+	private static Singleton instance;
+	private Singleton(){
+
+	}
+	public static Singleton getInstance(){
+		if (instance =null) {
+			
+		}
+		return instance;
+	}
+	public void print(){
+		System.out.println("www.baidu.com");
+	}
+}
+	
